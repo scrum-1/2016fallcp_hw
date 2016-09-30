4e340d7d2c82e2942c487b211134aed3e251cf0d
scrum-1 <scrum1@mde.tw>
2016-09-30 14:20:41 +0800

    Update w4.py

--- w4.py
+++ w4.py
@@ -1,12 +1,4 @@
-program = '''
 from browser import window
 my_account = "scrum-1"
-window.open().document.write(program +my_account)
-<br />
-<br />
-'''
-
-from browser import window
-my_account = "scrum-1"
-window.open().document.write(program +my_account)
+window.open().document.write(my_account)
 
